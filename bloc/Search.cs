d0c33e2b34200b53ed0dc60b8dd03e5aab5251ec
walumo <miikka.hakulinen@awacademy.training>
2022-07-05 19:10:47 +0300

    Added exception handling

--- bloc/Search.cs
+++ bloc/Search.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using FantasticFour.models;
+using FantasticFour.network;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using FantasticFour.network;
-using FantasticFour.models;
 using System.Threading.Tasks;
-using System.Text;
 
 namespace FantasticFour.bloc
 {
@@ -18,9 +18,17 @@ namespace FantasticFour.bloc
             {
                 return (from station in list where string.Equals(station.stationName, str, StringComparison.OrdinalIgnoreCase) select station.stationShortCode).First();
             }
-            catch (Exception ex)
+            catch (Exception e1)
             {
-                return (from station in list where Regex.IsMatch(station.stationName, str, RegexOptions.IgnoreCase) select station.stationShortCode).First();
+                File.AppendAllText(Environment.CurrentDirectory + "errorLog.txt", DateTime.Now + "| error converting station name: " + e1.Message + "\n");
+                try
+                {
+                    return (from station in list where Regex.IsMatch(station.stationName, str, RegexOptions.IgnoreCase) select station.stationShortCode).First();
+                }
+                catch (Exception e2)
+                {
+                    return "HKI";
+                }
             }
         }
         public static async Task<string> GetStationName()
@@ -31,19 +39,19 @@ namespace FantasticFour.bloc
             {
                 List<Station> sIndex = new List<Station>();
 
-                Console.Write("Search: "+searchString);
+                Console.Write("Search: " + searchString);
                 ConsoleKeyInfo letter = Console.ReadKey();
                 Console.Clear();
-                
-                if(!Regex.IsMatch(letter.KeyChar.ToString(), "[a-z]", RegexOptions.IgnoreCase)
-                    && letter.Key != ConsoleKey.Backspace 
+
+                if (!Regex.IsMatch(letter.KeyChar.ToString(), "[a-ö]", RegexOptions.IgnoreCase)
+                    && letter.Key != ConsoleKey.Backspace
                     && letter.Key != ConsoleKey.Enter
                     && letter.Key != ConsoleKey.Spacebar) continue;
 
                 if (letter.Key != ConsoleKey.Backspace && letter.Key != ConsoleKey.Enter) searchString += letter.KeyChar.ToString();
                 else if (letter.Key == ConsoleKey.Backspace && searchString.Count() > 0) searchString = searchString.Remove(searchString.Count() - 1, 1);
                 else if (letter.Key == ConsoleKey.Enter && !String.IsNullOrWhiteSpace(searchString)) return await GetShortStationName(searchString);
-                
+
 
                 if (!String.IsNullOrWhiteSpace(searchString))
                 {
@@ -65,7 +73,7 @@ namespace FantasticFour.bloc
                     }
                     else
                     {
-                        Console.WriteLine("Not found!");
+                        Console.WriteLine("Not found!\n");
                     }
                 }
             }
